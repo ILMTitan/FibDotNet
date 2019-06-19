e94de2ccecba8432bc89928fcbae5f40fa73aed7
ILMTitan <ILMTitan@outlook.com>
2019-06-19 15:37:11 -0700

    Supress more messages.

---
+++ Jib.Net.Core.Unit.Tests/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Formatting",
+    "RCS1029:Format binary operator on next line.",
+    Justification = "Formatting style I disagree with.")]
+
